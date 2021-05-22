a967cf20180fbca6552b4f56c16de38349e212e3
khafif jessy <khafifjessy@gmail.com>
2021-05-22 15:26:30 +0200

    problem 5 solved

---
+++ pb5.py
@@ -0,0 +1,23 @@
+def checkDivs(inf, sup, n):
+  
+  check = True
+  div = inf
+  
+  while( check and (div < (sup+1)) ):
+    check = (n%div == 0)
+    div += 1
+
+  return check
+
+def f():
+
+  inf = 1
+  sup = 20
+  n = sup
+  
+  while(not checkDivs(inf, sup, n)):
+    n += sup
+  
+  return n
+
+print(f())
