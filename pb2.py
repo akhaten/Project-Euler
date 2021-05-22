a10a776e0a68c6b5f0025f57e8670c2768b58577
khafif jessy <khafifjessy@gmail.com>
2021-05-22 12:50:18 +0200

    problem 2 solved

--- pb2.py
+++ pb2.py
@@ -1,15 +1,18 @@
-limit = 4000000
 
-# u0 = 1
-u0 = 1
-# u1 = 2
-u1 = 2
-# res = 2 because u1 % 2 == 0
-res = 2
+def f(n):
+  # u0 = 1
+  u0 = 1
+  # u1 = 2
+  u1 = 2
+  # res = 2 because u1 % 2 == 0
+  res = 2
 
-# forall n > 1, u(n) = u(n-1) + u(n-2)
-while(u1 <= limit):
-  u0, u1 = u1, u0 + u1 
-  if u1%2 == 0:
-    res = res + u1
-print("res pb1 = ", res)
+  # forall n > 1, u(n) = u(n-1) + u(n-2)
+  while(u1 <= n):
+    u0, u1 = u1, u0 + u1 
+    if u1%2 == 0:
+      res = res + u1
+  
+  return res
+
+print(f(4000000))
