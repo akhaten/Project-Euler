08c61e53c2a0b1e9f6cf45b5b52ffd8d3afc036a
khafif jessy <khafifjessy@gmail.com>
2021-05-22 18:07:54 +0200

    problem solved

---
+++ pb9.py
@@ -0,0 +1,18 @@
+# https://projecteuler.net/problem=9
+
+def checkSumTriplet(a, b, c):
+  return a+b+c == 1000
+
+def checkDefTriplet(a, b, c):
+  return (a < b < c) and (a**2 + b**2 == c**2)
+
+def findPythagoreanTriplet(n):
+  for c in range(3, n):
+    for b in range(2, c):
+      for a in range(1, b):
+        if  checkDefTriplet(a, b, c) and checkSumTriplet(a, b, c) :
+          return a*b*c
+
+
+print(findPythagoreanTriplet(1000))
+          
