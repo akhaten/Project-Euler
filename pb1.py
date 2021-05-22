cd62e08091157be1ce25aa674f42b61484daa46f
khafif jessy <khafifjessy@gmail.com>
2021-05-22 12:49:45 +0200

    problem 1 solved

--- pb1.py
+++ pb1.py
@@ -1,6 +1,12 @@
-limit = 1000
-res = 0
-for i in range(0, limit):
-  if (i%3 == 0) or (i%5 == 0):
-    res += i
-print("res pb1 = ", res)
+
+def f(n):
+  res = 0
+
+  for i in range(0, n):
+    if (i%3 == 0) or (i%5 == 0):
+      res += i
+  
+  return res
+    
+
+print(f(1000))
